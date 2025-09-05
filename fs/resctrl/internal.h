1fbbf4eec8eaec4658da13d233fb0f54ee9af972
Babu Moger <babu.moger@amd.com>
2025-09-05 16:34:16 -0500

    fs/resctrl: Add the functionality to assign MBM events
    commit bd85310efd71b9e7809e1b95fe7a60fde42e62db upstream.
    
    When supported, "mbm_event" counter assignment mode offers "num_mbm_cntrs"
    number of counters that can be assigned to RMID, event pairs and monitor
    bandwidth usage as long as it is assigned.
    
    Add the functionality to allocate and assign a counter to an RMID, event
    pair in the domain. Also, add the helper rdtgroup_assign_cntrs() to assign
    counters in the group.
    
    Log the error message "Failed to allocate counter for <event> in domain
    <id>" in /sys/fs/resctrl/info/last_cmd_status if all the counters are in
    use. Exit on the first failure when assigning counters across all the
    domains.
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Link: https://lore.kernel.org/cover.1757108044.git.babu.moger@amd.com
    
     Conflicts:
    	fs/resctrl/internal.h
    	fs/resctrl/monitor.c
    [jz: resolve context conflicts]
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- fs/resctrl/internal.h
+++ fs/resctrl/internal.h
@@ -387,6 +387,8 @@ bool closid_allocated(unsigned int closid);
 
 int resctrl_find_cleanest_closid(void);
 
+void rdtgroup_assign_cntrs(struct rdtgroup *rdtgrp);
+
 #ifdef CONFIG_RESCTRL_FS_PSEUDO_LOCK
 int rdtgroup_locksetup_enter(struct rdtgroup *rdtgrp);
 
