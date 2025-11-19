41ad98bbdf558cda12089b241a88fa0d5267dbdb
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2025-11-19 12:11:40 -0800

    EDAC/{skx_common,imh}: Add EDAC driver for Intel Diamond Rapids servers
    commit 9fc67b11703fe9d8a5617ccacec2a452e455fa52 upstream.
    
    Intel Diamond Rapids CPUs include Integrated Memory and I/O Hubs (IMH).
    The memory controllers within the IMHs provide memory stacks to the
    processor. Create a new driver for this IMH-based memory controllers
    rather than applying additional patches to the existing i10nm_edac.c
    for the following reasons:
    
    1) The memory controllers are not presented as PCI devices; instead,
       the detection and all their registers have been transitioned to
       MMIO-based memory spaces.
    
    2) Validation processes are costly. Modifications to i10nm_edac would
       require extensive validation checks against multiple platforms,
       including Ice Lake, Sapphire Rapids, Emerald Rapids, Granite Rapids,
       Sierra Forest, and Grand Ridge.
    
    3) Future Intel CPUs will likely only need patches on top of this new
       EDAC driver. Validation can be limited to Diamond Rapids servers
       and future Intel CPU generations.
    
    [Tony: Fix kerneldoc for struct local_reg]
    [randconfig: Added dependencies on NFIT and DMI]
    
    Intel-SIG: commit 9fc67b11703f EDAC/{skx_common,imh}: Add EDAC driver for Intel Diamond Rapids servers
    Backport EDAC driver for DMR
    
    Tested-by: Yi Lai <yi1.lai@intel.com>
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Link: https://patch.msgid.link/20251119134132.2389472-5-qiuxu.zhuo@intel.com
    [ Zhang Rui: amend commit log (build fix) ]
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>

--- drivers/edac/skx_common.c
+++ drivers/edac/skx_common.c
@@ -124,7 +124,7 @@ void skx_adxl_put(void)
 }
 EXPORT_SYMBOL_GPL(skx_adxl_put);
 
-static void skx_init_mc_mapping(struct skx_dev *d)
+void skx_init_mc_mapping(struct skx_dev *d)
 {
 	/*
 	 * By default, the BIOS presents all memory controllers within each
@@ -135,6 +135,7 @@ static void skx_init_mc_mapping(struct skx_dev *d)
 	for (int i = 0; i < NUM_IMC; i++)
 		d->mc_mapping[i] = i;
 }
+EXPORT_SYMBOL_GPL(skx_init_mc_mapping);
 
 void skx_set_mc_mapping(struct skx_dev *d, u8 pmc, u8 lmc)
 {
@@ -835,7 +836,7 @@ EXPORT_SYMBOL_GPL(skx_remove);
 /*
  * Debug feature.
  * Exercise the address decode logic by writing an address to
- * /sys/kernel/debug/edac/{skx,i10nm}_test/addr.
+ * /sys/kernel/debug/edac/{skx,i10nm,imh}_test/addr.
  */
 static struct dentry *skx_test;
 
