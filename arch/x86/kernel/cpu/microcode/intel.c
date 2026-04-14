intel-lab-lkp/linux

x86/microcode/intel: Enable staging when available
With staging support implemented, enable it when the CPU reports the
feature.

Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
Tested-by: Anselm Busse <abusse@amazon.de>
Reviewed-by: Chao Gao <chao.gao@intel.com>
Reviewed-by: Tony Luck <tony.luck@intel.com>

arch/x86/kernel/cpu/microcode/intel.c
@@ -980,6 +980,18 @@ static __init void calc_llc_size_per_core(struct cpuinfo_x86 *c)
 	llc_size_per_core = (unsigned int)llc_size;
 }
 
+static __init bool staging_available(void)
+{
+	u64 val;
+
+	val = x86_read_arch_cap_msr();
+	if (!(val & ARCH_CAP_MCU_ENUM))
+		return false;
+
+	rdmsrq(MSR_IA32_MCU_ENUMERATION, val);
+	return !!(val & MCU_STAGING);
+}
+
 struct microcode_ops * __init init_intel_microcode(void)
 {
 	struct cpuinfo_x86 *c = &boot_cpu_data;
@@ -988,6 +1000,11 @@ struct microcode_ops * __init init_intel_microcode(void)
 	    cpu_has(c, X86_FEATURE_IA64)) {
 		pr_err("Intel CPU family 0x%x not supported\n", c->x86);
 		return NULL;
+	}
+
+	if (staging_available()) {
+		microcode_intel_ops.use_staging = true;
+		pr_info("Enabled staging feature.\n");
 	}
 
 	calc_llc_size_per_core(c);

openvelinux/kernel-intel

commit bffeb2fd0b9c99d8af348da88335bff408c63882 upstream.
  [ bp: Sort in the MSR properly. ]
Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
Link: https://lore.kernel.org/20250320234104.8288-1-chang.seok.bae@intel.com
Signed-off-by: Yongting Lin <linyongting@bytedance.com>

arch/x86/kernel/cpu/microcode/intel.c
@@ -990,6 +990,18 @@ static __init void calc_llc_size_per_core(struct cpuinfo_x86 *c)
 	llc_size_per_core = (unsigned int)llc_size;
 }
 
+static __init bool staging_available(void)
+{
+	u64 val;
+
+	val = x86_read_arch_cap_msr();
+	if (!(val & ARCH_CAP_MCU_ENUM))
+		return false;
+
+	rdmsrl(MSR_IA32_MCU_ENUMERATION, val);
+	return !!(val & MCU_STAGING);
+}
+
 struct microcode_ops * __init init_intel_microcode(void)
 {
 	struct cpuinfo_x86 *c = &boot_cpu_data;
@@ -998,6 +1010,11 @@ struct microcode_ops * __init init_intel_microcode(void)
 	    cpu_has(c, X86_FEATURE_IA64)) {
 		pr_err("Intel CPU family 0x%x not supported\n", c->x86);
 		return NULL;
+	}
+
+	if (staging_available()) {
+		microcode_intel_ops.use_staging = true;
+		pr_info("Enabled staging feature.\n");
 	}
 
 	calc_llc_size_per_core(c);